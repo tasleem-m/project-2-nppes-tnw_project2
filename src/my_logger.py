0f4ff294f3e776e6fba6db8332ca9abcde03f3f8
Tasleem M <trmuhammad2017@gmail.com>
2026-02-06 22:45:57 -0600

    Add files via upload

--- src/my_logger.py
+++ src/my_logger.py
@@ -1,1 +1,31 @@
+import logging
+from pathlib import Path
 
+Path("logs").mkdir(exist_ok=True)
+
+def get_logger(name: str = "basic_logger") -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    if not logger.handlers:
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)
+
+        file_handler = logging.FileHandler("logs/basic.log", mode="a")
+        file_handler.setLevel(logging.ERROR)
+
+        console_format = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        file_format = logging.Formatter(
+            "{'time':'%(asctime)s', 'name': '%(name)s', "
+            "'level': '%(levelname)s', 'message': '%(message)s'}"
+        )
+
+        console_handler.setFormatter(console_format)
+        file_handler.setFormatter(file_format)
+
+        logger.addHandler(console_handler)
+        logger.addHandler(file_handler)
+
+    return logger
