2cf22b4696f272d16302838180c62a23cfcf8db7
Tasleem M <trmuhammad2017@gmail.com>
2026-02-06 22:52:31 -0600

    Add files via upload

--- project_2_nppes/models/marts/provider_density.sql
+++ project_2_nppes/models/marts/provider_density.sql
@@ -1,1 +1,6 @@
-
+SELECT ROUND((COUNT(pr.Taxonomy_Code) / pop.POP)*10000) AS provider_per_capita, county_name, state_abbv
+FROM {{ ref('provider_directory') }} AS pr
+INNER JOIN {{ ref('population_clean') }} AS pop
+USING (county_name, state_abbv)
+GROUP BY pop.POP, county_name, state_abbv
+ORDER BY county_name
