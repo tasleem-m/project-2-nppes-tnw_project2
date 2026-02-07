f4aff49f053b5f2059a31178188b18642e5b6244
Tasleem M <trmuhammad2017@gmail.com>
2026-02-06 22:42:17 -0600

    Add files via upload

--- src/utils/duckdb_helper.py
+++ src/utils/duckdb_helper.py
@@ -1,1 +1,14 @@
+import duckdb
 
+def convert_to_duckdb(schema, table_name, data, logger):
+    conn = duckdb.connect('project_2_nppes/dev.duckdb')
+
+    conn.execute(f"CREATE SCHEMA IF NOT EXISTS {schema}")
+
+    conn.execute(f"CREATE OR REPLACE TABLE {schema}.{table_name} AS SELECT * FROM data")
+
+    logger.info(f"DuckDB table {schema}.{table_name} created and/or updated")
+
+    print(conn.execute(f"SHOW TABLES FROM {schema}").fetchall())
+
+    conn.close()
