2a209ee05269a399d320f7c4551657a8be089795
Tasleem M <trmuhammad2017@gmail.com>
2026-02-06 22:53:16 -0600

    Add files via upload

--- project_2_nppes/models/marts/provider_directory.sql
+++ project_2_nppes/models/marts/provider_directory.sql
@@ -1,1 +1,8 @@
-
+SELECT n.provider_name, p.county_name, n.Provider_Business_Practice_Location_Address_City_Name AS city_name, n.state_abbv, n.ZIP, s.Classification, n.Taxonomy_Code, z.county_code
+FROM {{ ref('nppes_clean') }} AS n
+INNER JOIN {{ ref('zip_county_clean') }} AS z
+USING (ZIP)
+INNER JOIN {{ ref('population_clean') }} AS p
+USING (county_code)
+INNER JOIN {{ ref('specialties_clean') }} AS s
+USING (Taxonomy_Code)
