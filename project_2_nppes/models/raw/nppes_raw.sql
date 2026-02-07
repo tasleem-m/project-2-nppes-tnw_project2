b80b25bf03060dc3106afc252631a9f4506f90b9
Tasleem M <trmuhammad2017@gmail.com>
2026-02-06 22:55:13 -0600

    Add files via upload

--- project_2_nppes/models/raw/nppes_raw.sql
+++ project_2_nppes/models/raw/nppes_raw.sql
@@ -1,1 +1,5 @@
-
+select "NPI", "Provider Organization Name (Legal Business Name)", 
+                  "Provider Last Name (Legal Name)", "Provider First Name", "Provider Middle Name", 
+                  "Provider Business Practice Location Address City Name", "Provider Business Practice Location Address State Name", 
+                  "Provider Business Practice Location Address Postal Code", "Healthcare Provider Taxonomy Code_1"
+from {{ source('raw', 'nppes_raw') }}
