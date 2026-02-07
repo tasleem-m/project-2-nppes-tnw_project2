115b4bc74b282e558776a77bcb21dad26b8b1f34
Tasleem M <trmuhammad2017@gmail.com>
2026-02-06 22:54:00 -0600

    Add files via upload

--- project_2_nppes/models/marts/providers_by_geography.sql
+++ project_2_nppes/models/marts/providers_by_geography.sql
@@ -1,1 +1,6 @@
-
+SELECT COUNT(n.NPI) AS provider_count, n.state_abbv
+FROM {{ ref('nppes_clean') }} AS n
+INNER JOIN {{ ref('states_clean') }} AS s
+ON n.state_abbv = s.state
+GROUP BY n.state_abbv
+ORDER BY provider_count DESC
