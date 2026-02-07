fe5f59e933f4b2d70fb16c8b93ddc6fe047af732
Tasleem M <trmuhammad2017@gmail.com>
2026-02-06 22:50:29 -0600

    Add files via upload

--- project_2_nppes/macros/distinct_providers_by_geography.sql
+++ project_2_nppes/macros/distinct_providers_by_geography.sql
@@ -1,1 +1,8 @@
+{% test distinct_providers_by_geography(model) %}
 
+SELECT provider_name, county_name, city_name, state_abbv, COUNT(Taxonomy_Code) AS occurrences
+FROM {{ model }}
+GROUP BY provider_name, county_name, city_name, state_abbv
+HAVING COUNT(Taxonomy_Code) > 1
+
+{% endtest %}
